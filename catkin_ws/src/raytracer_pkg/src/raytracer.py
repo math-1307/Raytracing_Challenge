dcb6b1dd56362be9c59236ca59836994c4bb4b58
Demogorgon <mathew137@ymail.com>
2022-08-24 14:39:21 +0200

    commit

--- catkin_ws/src/raytracer_pkg/src/raytracer.py
+++ catkin_ws/src/raytracer_pkg/src/raytracer.py
@@ -1,20 +1,21 @@
 #!/usr/bin/env python
 
+import rospy
 import tf
 import tf2_ros
-from tf.transformations import quaternion_matrix
-import rospy
-from visualization_msgs.msg import Marker, MarkerArray
-from geometry_msgs.msg import Point, Quaternion
-from sensor_msgs.msg import PointCloud2, PointField
-from std_msgs.msg import Header
 import numpy as np
 import sensor_msgs.point_cloud2 as pc
+
+from tf.transformations import quaternion_matrix
+
+from visualization_msgs.msg import Marker, MarkerArray
+from geometry_msgs.msg import Point, Quaternion, Pose
+from sensor_msgs.msg import PointCloud2, PointField
+from std_msgs.msg import Header
+
 from dynamic_reconfigure.server import Server
 from raytracer_pkg.cfg import lidar_poseConfig
 
-from raytracer_pkg.msg import represent_plane, represent_ray, rayArray
-#from raytracer_pkg import LiDAR
 
 class LiDAR:
 
@@ -24,7 +25,9 @@ class LiDAR:
 		self.lidar_id = lidar_num
 		
 		lidar_topic = "/lidar"+str(self.lidar_id)
+		ray_topic = "/ray"+str(self.lidar_id)
 		self.pub_lidar_marker = rospy.Publisher(lidar_topic, Marker, queue_size=1)
+		#self.pub_rays = rospy.Publisher(ray_topic, MarkerArray, queue_size=1)
 
 		# Marker Properties
 		self.lidar_marker = Marker()
@@ -64,6 +67,7 @@ class LiDAR:
 		self.defineRays()
 		
 		self.pub_lidar_marker.publish(self.lidar_marker)
+		#self.pub_rays.publish(self.rays_marker)
 		
 	def defineRays(self):
 		
@@ -76,14 +80,42 @@ class LiDAR:
 		y = 1 * np.tan(np.deg2rad(self.angles["horizontal"]))
 		z = 1 * np.tan(np.deg2rad(self.angles["vertical"]))
 		yv, zv = np.meshgrid(y, z)
-		pts_array = np.append(np.ones((yv.size,1)),(np.append(yv.reshape(yv.size,1),zv.reshape(zv.size,1),axis=1)),axis=1)
+		pts_array = np.append(np.ones((yv.size,1)),(np.append(yv.reshape(yv.size,1),zv.reshape(zv.size,1),axis=1)),axis=1) #600x3
 		
-		# depicts rays (directional vector calculated from calibration plane)		
+		# depicts rays (origin and directional vectors calculated from calibration plane)		
 		self.rays = np.append(np.zeros((int(pts_array.size/3),3)),pts_array,axis=1)
 		
-		#mag = (np.sqrt((np.sum(np.multiply(self.rays,self.rays), axis = 1)).reshape(-1,1)))
-		#self.rays = np.divide(self.rays,mag)
+		# Array of Markers for rays
+		self.rays_marker = MarkerArray()
+		rpose = Pose()
+		for i in range(pts_array.shape[0]):
+
+			q = tf.transformations.quaternion_from_euler(np.deg2rad(pts_array[i,0]), np.deg2rad(pts_array[i,1]), np.deg2rad(pts_array[i,2]))	
 		
+			rm = Marker()
+			rm.ns = "ray"+str(i+1)
+			rm.header.frame_id = self.lidar_frame
+			rm.id = i+1
+			rm.header.stamp = rospy.get_rostime()
+			rm.type = Marker.ARROW
+			rm.action = Marker.ADD
+			rm.lifetime = rospy.Duration(0)
+			rm.scale.x = 30.0
+			rm.scale.y = 0.01
+			rm.scale.z = 0.01
+			rm.color.r = 1.0
+			rm.color.g = 0.0
+			rm.color.b = 0.0
+			rm.color.a = 1.0
+			rm.pose.position.x = 0
+			rm.pose.position.y = 0
+			rm.pose.position.z = 0
+			rm.pose.orientation.x = q[0]
+			rm.pose.orientation.y = q[1]
+			rm.pose.orientation.z = q[2]
+			rm.pose.orientation.w = q[3]
+			
+			self.rays_marker.markers.append(rm)
 class Wall:
 
 	def __init__(self):
@@ -132,14 +164,16 @@ class Wall:
 		
 	def depictPlane(self):
 		
+		# To depict the plane, corner points and normal vector are calculated
+		# Corners
 		y,z = np.meshgrid([-self.dim_y/2,self.dim_y/2],[-self.dim_z/2,self.dim_z/2])
 		self.corners = np.append(-0.25*np.ones(((y.size,1))), np.append(y.reshape((y.size,1)),z.reshape((y.size,1)),1),1) # 4x3 matrix
-
+		
+		# Normal Vector
 		vec1 = np.asarray([self.corners[1,0]-self.corners[0,0], self.corners[1,1]-self.corners[0,1], self.corners[1,2]-self.corners[0,2]])
 		vec2 = np.asarray([self.corners[2,0]-self.corners[0,0], self.corners[2,1]-self.corners[0,1], self.corners[2,2]-self.corners[0,2]])
 		self.normal = np.cross(vec1,vec2).reshape(3,1)
-		#self.normal = self.normal / np.linalg.norm(self.normal)
-		#print("normalised normal " + str(self.normal))
+
 
 class PointCloud:
 
@@ -147,23 +181,17 @@ class PointCloud:
 
 		pcd_id = lidar_obj.lidar_id
 		
-		#self.cloud = PointCloud2()
-		#self.cloud = self.array_to_pc2(Rays.calibration_pts, rospy.get_rostime(), "lidar_frame")		
-		#self.pub_pcd.publish(self.cloud)
-		
 		self.calc_POI(lidar_obj, wall_obj, tf_buffer)
 		
 		#self.POI.reshape(-1,3)
 			
 		
-	def calc_POI(self,lidar_obj, plane, tf_buffer):
-
-#		listener.waitForTransform('/lidar_frame','/wall_frame',rospy.Time(), rospy.Duration(4.0))	
+	def calc_POI(self,lidar_obj, plane, tf_buffer):	
 
 		try:
-			#print(lidar_obj.lidar_frame)
+			
 			trans = tf_buffer.lookup_transform(lidar_obj.lidar_frame, 'wall_frame', rospy.Time())#, rospy.Duration(1.0)) # frames from, to, time
-#			print(trans.transform.rotation)
+
 			q = [trans.transform.rotation.x, trans.transform.rotation.y, trans.transform.rotation.z, trans.transform.rotation.w]
 			tr_mat = np.matrix(quaternion_matrix(q))
 			tr_mat[0,3] = trans.transform.translation.x
@@ -173,35 +201,33 @@ class PointCloud:
 			tr_mat = np.asarray(tr_mat).reshape(4,4)
 
 
-			rays = lidar_obj.rays
-						
+			rays = lidar_obj.rays			
 			crnr = plane.corners
 			
+			# Corners of plane in the LiDAR frame
 			crnr = np.append((crnr.T),np.ones((1,crnr.shape[0])),axis=0)
 			crnr = tr_mat @ crnr # 4x4
 			crnr = crnr / crnr[-1,:] # 4x4
 			crnr = crnr[0:-1,:].T # 4x3
-						
-			tra = 0
+			
 			crnr1 = np.tile(crnr[0,:],(rays.shape[0],1))
 			
+			# Normal vector to the plane in the LiDAR frame
 			nrm = plane.normal.reshape(3,1)
-
 			rot = tr_mat[0:3,0:3]
-			nrm =  rot @ nrm
-								
+			nrm =  rot @ nrm				
 			nrm = np.tile(nrm.reshape(1,3),(rays.shape[0],1))
-		
+			
 			nr = ((crnr1 - rays[:,0:3]) @ nrm.T).diagonal()
 			dr = (rays[:,3:6] @ nrm.T).diagonal()
-
+			
+			# Parametric distance between plane and rays
 			t = (nr/dr).reshape(-1,1)
 			t = np.tile(t,(1,3))
-
-			poi = (rays[:,0:3]) + np.multiply(t, rays[:,3:6])
 			
+			# Points of Intersection
+			poi = (rays[:,0:3]) + np.multiply(t, rays[:,3:6])
 			poi = poi.tolist()
-
 			poi = np.asarray(poi)
 			
 			try:
@@ -212,6 +238,7 @@ class PointCloud:
 				tr2wrld[1,3] = trans2.transform.translation.y
 				tr2wrld[2,3] = trans2.transform.translation.z
 				
+				# Points of Intersection in the world frame
 				poiw = np.append((poi.T),np.ones((1,poi.shape[0])),axis=0) # 4x600
 				poiw = tr2wrld @ poiw # 4x600
 				poiw = poiw / poiw[-1,:] # 4x600
@@ -219,16 +246,12 @@ class PointCloud:
 				poiw = poiw.tolist()
 				poiw = np.asarray(poiw)
 				
+				# Corners in the world frame
 				crnrw = np.append((crnr.T),np.ones((1,crnr.shape[0])),axis=0)
 				crnrw = tr2wrld @ crnrw # 4x4
 				crnrw = crnrw / crnrw[-1,:] # 4x4
 				crnrw = crnrw[0:-1,:].T # 4x3
 				
-				#poiw = np.around(poiw,2)
-				#poiw = poiw[(poiw[:,1]>=int(np.min(crnrw[:,1]))) & (poiw[:,1]>=np.max(crnrw[:,1]))] # setting y limits
-				#poiw = poiw[(poiw[:,2]>=int(np.min(crnrw[:,2]))) & (poiw[:,2]>=int(np.max(crnrw[:,2])))] # setting z limits
-				
-				# Store pointcloud2 for all three LiDARs
 				new_arr = np.empty((0,3))
 				for pt in range(poiw.shape[0]):
 					if((poiw[pt,1] >= np.min(crnrw[:,1])) and (poiw[pt,1] <= np.max(crnrw[:,1]))):
@@ -241,11 +264,11 @@ class PointCloud:
 				else: self.POI = None
 				
 			except (tf2_ros.LookupException, tf2_ros.ConnectivityException, tf2_ros.ExtrapolationException):
-				print("Transforms from wall to world not yet available")
+				print("Transforms from world to "+ lidar_obj.lidar_frame + " not yet available")
 				self.POI = None
 				
 		except (tf2_ros.LookupException, tf2_ros.ConnectivityException, tf2_ros.ExtrapolationException):
-			print("Transforms from world to " + lidar_obj.lidar_frame + " not yet available")
+			print("Transforms from " + lidar_obj.lidar_frame + " to wall not yet available")
 			self.POI = None			
 
 
@@ -297,24 +320,18 @@ if __name__ == '__main__':
 
 	rospy.init_node('raytracer_node', anonymous=True)
 	
+	# To obtain transforms
 	tf_buffer = tf2_ros.Buffer()
 	tf2_listener = tf2_ros.TransformListener(tf_buffer)
 	
 	# for Dynamic Reconfiguration
 	drc = Server(lidar_poseConfig,drcCallback)
 	
-	rate = rospy.Rate(10) # 10hz
+	rate = rospy.Rate(100) # 10hz
 	
 	
 	#lidar_poses = np.array([[0.5,0,1,0,2,0],[0,1,1,-5,2,10],[0,-1,1,5,2,-10]])
 
-
-	#lid_q = tf.transformations.quaternion_from_euler(np.deg2rad(0), np.deg2rad(2), np.deg2rad(0))
-	
-	
-	#lidar_poses = np.array([[0.5,0,1,0,2,0],[0,1,1,-5,2,45],[0,-1,1,5,2,-45]])
-	#lidar_poses = np.array([[0.5,0,1,0,0,0],[0,1,1,0,0,0],[0,-1,1,0,0,0]])
-	
 	while not rospy.is_shutdown():  
 	       
 		info = "Started publishing markers for visualization at %s" % rospy.get_time()
@@ -339,11 +356,7 @@ if __name__ == '__main__':
 									np.deg2rad(lidar_poses[lid,5]))
 			
 			lid_br = tf.TransformBroadcaster()
-#			lid_br.sendTransform((lidar_poses[lid,0], lidar_poses[lid,1], lidar_poses[lid,2]),
-#					tuple(lid_q),
-#					rospy.get_rostime(),
-#					lidar.lidar_frame,
-#					"/world")
+
 			lid_br.sendTransform((lidar_poses[lid,0], lidar_poses[lid,1], lidar_poses[lid,2]),
 					(lid_q[0],lid_q[1],lid_q[2],lid_q[3]),
 					rospy.get_rostime(),
@@ -351,6 +364,8 @@ if __name__ == '__main__':
 					"/world")
 			
 			pcArr = PointCloud(lidar,wall1,tf_buffer)
+			
+			# Store pointcloud2 for all three LiDARs
 			if pcArr.POI is not None:
 				combinedPOI = np.append(combinedPOI, np.array(pcArr.POI).reshape(-1,3), axis = 0)
 			
@@ -359,7 +374,7 @@ if __name__ == '__main__':
 		pcd_topic = "/pcd_lidar"
 		pub_pcd = rospy.Publisher(pcd_topic, PointCloud2, queue_size=1)
 		print("Point Cloud Data of shape " + str(combinedPOI.shape) + " is being visualized")
-		#print(combinedPOI)
+		
 		if combinedPOI.any():
 			cloud = PointCloud2()
 			cloud = array_to_pc2(combinedPOI, rospy.get_rostime(), 'world')	
@@ -367,4 +382,3 @@ if __name__ == '__main__':
 
 		rate.sleep()
 
-	
